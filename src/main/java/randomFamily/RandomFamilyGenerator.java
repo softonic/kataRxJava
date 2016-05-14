3ea6a04cc4ced12996a2b1f3aed8de950d21cdbd
Jorge De Los Santos <jorgeluk13@gmail.com>
2016-05-14 14:04:24 +0200

    RandomFamilyGeneratorTest tests passing.

--- src/main/java/randomFamily/RandomFamilyGenerator.java
+++ src/main/java/randomFamily/RandomFamilyGenerator.java
@@ -1,5 +1,9 @@
 package randomFamily;
 
+import java.util.ArrayList;
+import java.util.List;
+import rx.functions.Func1;
+
 public class RandomFamilyGenerator {
   private final RandomUserApi randomUserApi;
 
@@ -8,8 +12,41 @@ public class RandomFamilyGenerator {
   }
 
   public FamilyEntity createFamily(int sons, int daughters) {
-    //TODO: Using the Random User API and the RxJava facilities, construct a new instance of RandomFamilyGenerator.
-    //Keep in mind to follow the anglo-saxon tradition related to the last name of the family members.
-    return null;
+
+    final UserEntity father = randomUserApi.getUserByGender(UserEntity.GENDER_MALE)
+        .map(new Func1<RequestEntity, UserEntity>() {
+          @Override public UserEntity call(RequestEntity requestEntity) {
+            return requestEntity.getResults().get(0);
+          }
+        })
+        .toBlocking()
+        .first();
+
+    UserEntity mother =
+        createNonFatherFamilyMember(UserEntity.GENDER_FEMALE, father.getName().getLast());
+
+    List<UserEntity> sonList = new ArrayList<>();
+    if (sons > 0) {
+      for (int i = 0; i < sons; i++) {
+        sonList.add(
+            createNonFatherFamilyMember(UserEntity.GENDER_MALE, father.getName().getLast()));
+      }
+    }
+    List<UserEntity> daughterList = new ArrayList<>();
+    if (daughters > 0) {
+      daughterList.add(
+          createNonFatherFamilyMember(UserEntity.GENDER_FEMALE, father.getName().getLast()));
+    }
+    return new FamilyEntity(father, mother, sonList, daughterList);
+  }
+
+  private UserEntity createNonFatherFamilyMember(String gender, final String familyName) {
+    return randomUserApi.getUserByGender(gender).map(new Func1<RequestEntity, UserEntity>() {
+      @Override public UserEntity call(RequestEntity requestEntity) {
+        UserEntity userEntity = requestEntity.getResults().get(0);
+        userEntity.getName().setLast(familyName);
+        return userEntity;
+      }
+    }).toBlocking().first();
   }
 }
